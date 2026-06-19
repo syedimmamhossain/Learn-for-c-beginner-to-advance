172d4dde3a041563cb1414fab98851f45c11b553
Syedimmamhossain <syedimmamhossain01@gmail.com>
2026-06-20 00:18:01 +0600

    Add condition1.c temperature code

--- week-2/for-loop.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int sum = 0;
-    for(int i=0; i<100; i++) {
-        sum+=i;
-    }
-    printf("the sum of the first 100 numbers is %d\n", sum);
-    return 0;
-}
